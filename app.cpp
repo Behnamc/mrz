e78370584dcf2b712108ebf3d4d5b43ed77a4657
Behnamc <85868653+Behnamc@users.noreply.github.com>
2022-06-08 18:34:37 +0430

    first commit

--- app.cpp
+++ app.cpp
@@ -1,7 +1,34 @@
-#include <iostream>
-using namespace std;
+#include <stdio.h>
+int f (int , int );
 
 int main()
 {
-    cout << "Welcome";
+    int  l , r , x , k , d , T = 0 ;
+    scanf("%d%d%d",&l,&r,&d);
+    for (int i = l ; i <= r ; i++)
+        T+=f(i,d);
+        //printf("%d\n",f(i,d));
+        printf("%d",T);
+}
+
+int f (int x , int k )
+{
+    int a[100]={0} ;
+    a[0]=x;
+    if (x%2==0)
+        a[1]=x/2;
+    else
+        a[1]=x+k;
+
+    for (int i = 2 ; i<=100 ; i++)
+    {
+        if (a[i-1]%2==0)
+            a[i]=a[i-1]/2;
+        else
+            a[i]=a[i-1]+k;
+    }
+
+    for (int i = 0 ; i<=100 ; i++)
+        if (a[i]<=k)
+            return i ;
 }
